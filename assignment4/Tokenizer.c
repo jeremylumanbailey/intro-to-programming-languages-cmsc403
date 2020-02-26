9f61315035062abef9187561d0f4924943cf9a7b
Jeremy <jeremylumanbailey@gmail.com>
2020-02-26 15:40:42 -0500

    backup tokenizer might be working

--- assignment4/Tokenizer.c
+++ assignment4/Tokenizer.c
@@ -10,41 +10,94 @@ _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf) {
     char currentLine[LEXEME_MAX];
     char fullline[LEXEME_MAX];
     char tmparr[LEXEME_MAX];
+
     char leftPar[1] = "(";
     char rightPar[1] = ")";
  
-    int count, countlex, c;
+    int i, count, countlex, c;
 
-    int i = 0;
+    //fgets parse file line by line putting each line into currentLine array
     while ( fgets(currentLine, MY_CHAR_MAX, inf) != NULL ) {
 
+        //Parse current line one char at a time putting each char in fullline array
         for(i=0; i<LEXEME_MAX; i++) {
             if( currentLine[i] == '\n'){
+                //end of line detected? Add terminator to full line
                 fullline[i] = '\0';
                 break;
             }
             fullline[i] = currentLine[i];
             count++;
         }
-        //count is = to the number of characters in that line
-        // printf("%s", fullline);
+
+//        printf("%s", fullline);
 
         for(i=0; i<count; i++){
-            if( fullline[i] == ' ' || !isalnum(fullline[i])) {
+
+            // if(isspace(fullline[i])) {
+            //     continue;
+            // }
+            
+            if( !isalnum(fullline[i]) && c > 0 && isalnum(tmparr[c-1]) ){
                 tmparr[c] = '\0';
-                if (tmparr[0] != '\0'){
-                    printf("%s\n", tmparr);
-                    //send tmparr to lex struct
-                    countlex++;
-                }
+                strcpy(aLex[countlex].lexeme, tmparr);
+                countlex++;
                 c = 0;
+                if( isspace(fullline[i]) ) {
+                  continue;
+                }
+
             }
-            if (fullline[i] != ' '){
+
+            
+            if ( !isalnum(fullline[i]) && !isspace(fullline[i]) ) {
+
+                // char fulllinei = fullline[i];
+                // char fullline2 = fullline[i+1];
+
+                // if( (strcmp( fulllinei, "!" ) == 0 || strcmp( fulllinei, "=" ) == 0) 
+                // && strcmp( fullline2, "=" ) == 0 )
+
+                if( (fullline[i] == '!' || fullline[i] == '=' ) && fullline[i+1] == '=' )
+
+                {
+                    tmparr[c] = fullline[i];
+                    tmparr[c+1] = fullline[i+1];
+                    tmparr[c+2] = '\0';
+                    strcpy(aLex[countlex].lexeme, tmparr);
+                    countlex++;
+                    c = 0;
+                    i++;
+                    continue;
+                }
+
+                else {
+                    tmparr[c] = fullline[i];
+                    tmparr[c+1] = '\0';
+                    strcpy(aLex[countlex].lexeme, tmparr);
+                    countlex++;
+                    c = 0;
+                    continue;
+                }
+
+            }
+
+
+
+            if (isalnum(fullline[i])) {
                 tmparr[c] = fullline[i];
                 c++;
+                continue;
             }
-        }
 
+
+
+
+
+        }
+    memset(fullline, '\0', sizeof(count));
+    count = 0;
+    c = 0;
     }
     *numLex = countlex;
     return TRUE;
